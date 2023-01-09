e2cf990f5c320ca43ecd12111e51327ec7b24440
7777777-4547 <7777777-4547@users.noreply.github.com>
2023-01-09 03:19:19 +0000

    deploy: 2348126e57955722d2ae050a7cd3bccd53736d21

---
+++ assets/update-logs_client-C.md.d2b617c0.js
@@ -0,0 +1,129 @@
+import{_ as s,c as n,o as a,a as l}from"./app.6ddf011e.js";const d=JSON.parse('{"title":"更新日志","titleTemplate":"客户端-C","description":"","frontmatter":{"title":"更新日志","layout":"docs","titleTemplate":"客户端-C"},"headers":[{"level":2,"title":"更新日志","slug":"更新日志","link":"#更新日志","children":[]}],"relativePath":"update-logs/client-C.md","lastUpdated":1673234288000}'),p={name:"update-logs/client-C.md"},e=l(`<h2 id="更新日志" tabindex="-1">更新日志 <a class="header-anchor" href="#更新日志" aria-hidden="true">#</a></h2><p>普通版于版本<code>v1.5.9</code>开始记载</p><hr><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-palenight"><code><span class="line"><span style="color:#A6ACCD;">v1.5.17_Hot2</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - SCL启动器1.1.0更新，修复JVM参数问题。因补上。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.17_Hot1</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新MoreCulling到0.12.4，以防崩溃。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新钠 · 扩展到0.4.16。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 调整相关光影配置。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.17</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加Keybind Fix，用来解决一定程度上的按键冲突。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加MixinTrace，用于崩溃日志打印Mixin使用情况。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Better Statistics Screen到2.1.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Connectivity到3.7。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fabric API到0.70.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fabric Loader到0.14.12。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新FastLoad到2.4.4。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新ImmediatelyFast到1.1.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Lambda的动态光源到2.2.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新More Culling到0.12.3。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Xaero的世界地图到1.28.7。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Xaero的小地图到22.17.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新更好的任务栏到1.5.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新钠 · 扩展到0.4.15。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 因More Culling更新，代替Cull Clouds的功能，因此移除。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 因More Culling更新，代替FeyTweaks的功能，因此移除。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - Too Many Players作用不大，因此移除。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.16</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新better stats到2.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Entity Texture Features到4.2.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fabric API到0.68.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fabric Language Kotlin到1.8.7（Kotlin.1.7.22）。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新ImmedialyFast到1.0.6。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Iris到1.4.5。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Xaero的世界地图到1.28.4。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Xaero的小地图到22.16.3。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新模组菜单到4.1.2。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新铁氧体磁芯到5.0.3。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 移除Raise Sound Limit Simplified，不稳定。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.15</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：AdaptiveTooltips，代替冰山和scrollable tooltips两个mod的功能。但需要先按Alt才可以滚动。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：BetterStats，代替原版的统计界面。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：Cull Clouds，优化云（高质量）的渲染机制。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：MidnightControls，给手柄做了基本支持。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：Recipe Book is Pain，把配方书的合成配方分类按创造模式物品栏的分类进行分类</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 将Fastbamboo的优化集成到钠中。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Architectury到6.3.49。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新ElytraPhysics到1.1.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Entity Texture Features到4.1.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fast Load到2.3.5。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - （又双）把Farslight换为Hold That Chunk，但是版本为2.0.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新ImmediatelyFast到1.0.5。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Iris到1.4.3正式版。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Reese的钠视频界面到1.4.9。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Xaero的世界地图到1.28.3。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Xaero的小地图到22.16.2。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新YetAnotherConfigLib到1.7.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新钠 · 拓展到0.4.14。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fabric Loader到0.14.11。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 暂时关掉游戏内输入法。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.14_Hot1</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Debugify到2.8.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新YetAnotherConfigLib到1.6.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.14</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：fastbamboo，优化竹子的计算。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：GraphUtil，改良调试界面的帧数图表显示。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：ImmediatelyFast，优化一些GUI、一些文字、方块实体、实体和粒子的渲染。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 再次添加回FsatAnim，优化实体模型计算。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：Sneaky，解决了打开背包时蹲下状态被取消的问题。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：Enhanced Attack Indicator，攻击提示的用处更多了。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fabric loader到0.14.10。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fabric API到0.64.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fabrication到3.1.3。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fast Load到1.2.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Sodium Extra到0.4.11[dev，build.1291]。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Xaero的世界地图到1.28.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Xaero的小地图到22.16.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新iris到1.5.0-shadowcomp-cu-test.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新方块实体优化到0.7.2。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 暂时关掉Exordium，因为稳定性不佳。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 一些配置更改。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 一些资源包更改。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新光影版本。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.13</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：冰山，横向过长的内容可以换到下一行。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：Feytweaks，可以智能剔除告示牌上的文字。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：Exordium，优化部分GUI和告示牌文字渲染。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：Sodium Fix，防止因光照导致的崩溃。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：Autoreconnect，掉线可以直接点重连了。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Debugify到2.7.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新iris到1.3.2-Beta2。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新钠 · 拓展到0.4.11。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 默认把Bobby关掉，使用Farsight，防止硬盘性能过拉影响帧率。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.12</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新YetAnotherConfigLib到1.5.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 默认关掉疾速加载，保证兼容和稳定性。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 移除Reese&#39;s Sodium Options Extra</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 使用No Prying Eyes 代替NoChatReports，聊天验证问题应该会没了。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新启动器SCL。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.11a</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更换Farsight为Bobby，并配置只保存7天内缓存区块，以免占用过高。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.11</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新YetAnotherConfigLib到1.2.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Cloth Config API到8.2.88。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Fabric API到0.61.0。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新夕夜材质包自动更新mod到1.1.2.9。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：Who am I，第三人称视角/背包视角可以看到自己的名字了。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：游戏内输入法，全屏也可以看到中文候选字了。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更换Enhanced Searchability为Recursive Resources。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.10</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新iris到1.3.1。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加新mod：Elytra Physics，鞘翅不会再那么僵硬了！</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">v1.5.9</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 支持滚动长出界面的工具提示。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更换另一个区块缓存mod[Farsight]。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新iris到1.3.0正式版。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 更新Debugify，由于此次变动，删掉了平滑滚动mod。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 添加EBE优化补丁资源包。</span></span>
+<span class="line"><span style="color:#A6ACCD;"> - 支持资源包搜索。</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,4),o=[e];function c(C,A,t,i,r,y){return a(),n("div",null,o)}const m=s(p,[["render",c]]);export{d as __pageData,m as default};
